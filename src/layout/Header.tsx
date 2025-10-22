579957d0c5391746d46fec13e31118b251bd85ac
aksel <aksel@metadot.com>
2025-10-21 19:39:42 -0500

    feat: implement first version of mobile menu

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -3,9 +3,12 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaBars } from "react-icons/fa";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
+  const toggleNav = () => setNavOpen(!navOpen);
   const dropdownRef = useRef<HTMLLIElement>(null);
 
   const toggleDropdown = () => setMenuOpen(!menuOpen);
@@ -44,7 +47,15 @@ export default function Header() {
             />
           </Link>
 
-          <ul className="navbar-nav">
+          <button
+            className="navbar-toggler lg:hidden relative cursor-pointer "
+            aria-label="Toggle navigation"
+            onClick={toggleNav}
+          >
+            <FaBars size={24} />
+          </button>
+
+          <ul className="navbar-nav hidden lg:flex">
             <li className="nav-item">
               <Link href="/about-us" className="nav-link">
                 About Us
@@ -132,6 +143,98 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+
+        <div className="  lg:hidden text-center">
+          <ul
+            className={`transition-all lg:hidden ${
+              navOpen
+                ? "max-h-500 visible"
+                : "max-h-0 invisible overflow-hidden"
+            }`}
+          >
+            <li className="nav-item p-[10px]">
+              <a href="/about-us" className="nav-link ">
+                About Us
+              </a>
+            </li>
+
+            <li className="nav-item p-[10px]">
+              <Link href="/contact" className="nav-link">
+                Contact
+              </Link>
+            </li>
+
+            <li ref={dropdownRef} className="nav-item p-[10px]">
+              <button
+                className="nav-link dropdown-toggle "
+                onClick={toggleDropdown}
+                aria-expanded={menuOpen}
+              >
+                Products
+              </button>
+
+              {menuOpen && (
+                <div className="dropdown-menu show">
+                  <Link href="https://bamzooka.com" className="dropdown-item">
+                    <Image
+                      src="/brand/bamzooka.webp"
+                      alt="Bamzooka"
+                      width={192}
+                      height={192}
+                      className="dropdown-image"
+                    />
+                    Bamzooka
+                  </Link>
+                  <Link
+                    href="https://www.daskeyboard.com"
+                    className="dropdown-item"
+                  >
+                    <Image
+                      src="/brand/dkbrand.webp"
+                      alt="Das Keyboard"
+                      width={49}
+                      height={63}
+                      className="dropdown-image"
+                    />
+                    Das Keyboard
+                  </Link>
+                  <Link
+                    href="https://www.mojohelpdesk.com"
+                    className="dropdown-item"
+                  >
+                    <Image
+                      src="/brand/mojo-star.svg"
+                      alt="Mojo Helpdesk"
+                      width={129}
+                      height={123}
+                      className="dropdown-image"
+                    />
+                    Mojo Helpdesk
+                  </Link>
+                  <Link
+                    href="https://www.montastic.com"
+                    className="dropdown-item"
+                  >
+                    <Image
+                      src="/brand/montastic.webp"
+                      alt="Montastic"
+                      width={75}
+                      height={64}
+                      className="dropdown-image"
+                    />
+                    Montastic
+                  </Link>
+                </div>
+              )}
+            </li>
+
+            <li className="nav-item p-[10px]">
+              <Link href="/blog" className="nav-link">
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
