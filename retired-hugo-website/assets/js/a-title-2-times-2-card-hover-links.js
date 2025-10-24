3460b0c6df067b8c80792a6f7306a4b25c4cd6fc
aksel <aksel@metadot.com>
2025-10-24 16:48:21 -0500

    chore: add retired hugo website in retired-hugp-website folder

---
+++ retired-hugo-website/assets/js/a-title-2-times-2-card-hover-links.js
@@ -0,0 +1,10 @@
+const aTitle2x2CardHoverLinks = new Swiper('.sc-cards-swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+    loop: true
+  });
---
+++ retired-hugo-website/content/blog/_index.md
@@ -0,0 +1,6 @@
+---
+title: "Latest News"
+# meta description
+description: "Read some news, tips and fact from Metadot."
+draft: false
+---
