ca070585b5b4bf896e90212d6d0f1f2587dbd260
fabiendv <dallavalle.fabien@gmail.com>
2026-02-04 15:26:31 +0100

    chore: update Next.js configuration and dependencies
    - Enable experimental MDX support in next.config.ts.
    - Update TypeScript configuration to use 'react-jsx' for JSX transformation.
    - Clean up sitemap-0.xml with updated last modification dates and added blog URLs.
    - Remove unnecessary peer dependencies from package-lock.json.

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,14 @@
 import type { NextConfig } from "next";
 import nextMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    mdxRs: true,
+  },
+};
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-  },
 });
 
 export default withMDX(nextConfig);
